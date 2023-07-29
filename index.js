4235c477084fd1f99399df3ecf15e76d3b0a9541
Ajay Priyadarshi <ajbj444@gmail.com>
2023-07-29 18:19:02 +0530

    Arrow button

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ videoCards.forEach((item) => {
 });
 
 //Card Slider
+
+let cardContainers = [...document.querySelectorAll(".card-container")];
+let preBtn = [...document.querySelectorAll(".pre-btn")];
+let nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+
+cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener("click", () => {
+        item.scrollLeft += containerWidth +200;
+    })
+
+    preBtn[i].addEventListener("click", () => {
+        item.scrollLeft -= containerWidth +200;
+    })
+})
+
